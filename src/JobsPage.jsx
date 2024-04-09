4b2a6cdd28c4bd81a9728291514dae38640c37e5
Thomas Kim <tkimm156@gmail.com>
2024-04-09 18:32:33 -0500

    Add docstring to JobsPage and work on search functionality

--- src/JobsPage.jsx
+++ src/JobsPage.jsx
@@ -2,18 +2,34 @@ import { useState, useEffect } from "react";
 import JoblyApi from "../api";
 import JobsList from "./JobsList";
 
-
+/** smart component to render jobs
+ *
+ * state:
+ * jobs - list of jobs like: [{{id, salary, title, equity, ...}, ...]
+ * isLoading - boolean representing status of api call
+ * isSearch - boolean representing to filter jobs or not based on user input
+ *
+ * props: none
+ *
+ * RouteList -> JobsPage -> JobList, SearchForm -> JobCard
+ */
 function JobsPage() {
     const [jobs, setJobs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [searchFilter, setSearchFilter] = useState("");
 
     useEffect(function fetchJobsWhenMounted() {
         async function fetchJobs() {
             const jobResponse = await JoblyApi.getJobs();
             setJobs(jobResponse);
+            setIsLoading(false);
         }
         fetchJobs();
+
     }, []);
 
+
+    if (isLoading) return <h1>Loading...</h1>;
 
     return (
         <div>
